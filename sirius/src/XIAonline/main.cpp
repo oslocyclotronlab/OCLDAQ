b8bd7a2b08e54589a6a553247a45f627ffc7b1e8
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2019-04-30 17:35:46 +0200

    Update main.cpp

--- sirius/src/XIAonline/main.cpp
+++ sirius/src/XIAonline/main.cpp
@@ -233,6 +233,7 @@ int main (int argc, char* argv[])
                 last_tus = tus;
 
                 data_p = unpacker->ParseBuffer(data, datalen, error);
+                std::cout << data_p.size() << std::endl;
                 sort_singles(data_p);
                 ++buffer_count;
                 evtbldr->SetBuffer(data_p);
