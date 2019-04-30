b94599c87298981ac9b39cf3f69c29da4c854c11
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2019-04-30 17:37:49 +0200

    Update Unpacker.cpp

--- sirius/src/XIAonline/Unpacker.cpp
+++ sirius/src/XIAonline/Unpacker.cpp
@@ -191,6 +191,7 @@ std::vector<word_t> Unpacker::ParseBuffer(const volatile uint32_t *buffer, const
             break;
 
         found.push_back(Extract_word(buffer+current_position, event_length, error));
+        current_position += event_length;
     }
 
     std::sort(found.begin(), found.end(), sort_func);
