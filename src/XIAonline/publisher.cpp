3b5ad4f8dd178a191977c22e27e87941c4e2f6ca
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2020-12-03 13:18:26 +0100

    Added publisher class

---
+++ src/XIAonline/publisher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vetle Wegner Ingeberg on 03/12/2020.
+//
+
+#include "publisher.h"
---
+++ src/XIAonline/publisher.h
@@ -0,0 +1,23 @@
+//
+// Created by Vetle Wegner Ingeberg on 03/12/2020.
+//
+
+#ifndef OCLDAQ_PUBLISHER_H
+#define OCLDAQ_PUBLISHER_H
+
+#include "zmq.hpp"
+
+/*!
+ * publisher class
+ * A simple class that takes waits for buffers from the unpacker and publishes them on the network.
+ */
+class publisher
+{
+
+private:
+
+
+};
+
+
+#endif //OCLDAQ_PUBLISHER_H
