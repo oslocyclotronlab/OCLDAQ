bb0b825cb47085184ffe395c6a307268125c931a
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2021-02-12 17:16:00 +0100

    Removed legacy code

--- sirius-legacy/reload_acq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-sudo killall -HUP XIAsort
-sudo ./buildit.sh
-acq_clear
-cd /mnt/current/240Pu-d-2018
-XIAsort
--- src/master/net_client.h
+++
@@ -1,8 +0,0 @@
-// -*- c++ -*-
-
-#ifndef NET_CLIENT_H
-#define NET_CLIENT_H 1
-
-int nc_connect(const char* hostname, int hostport);
-
-#endif /* NET_CLIENT_H */
