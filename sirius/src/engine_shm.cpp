c0703b0ab71ea39af54a9fd7f30b074c4f97a141
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2021-02-17 10:57:12 +0100

    Small update to the shm of engine.

--- sirius/src/engine_shm.cpp
+++ sirius/src/engine_shm.cpp
@@ -1,7 +1,7 @@
 
 #include "engine_shm.h"
 
-#include <string.h>
+#include <cstring>
 #include <sys/shm.h>
 
 static const int PERMS_WRITE = 0666; // read for all, write for all
@@ -25,13 +25,14 @@ unsigned int* engine_shm_attach(bool write)
     const int shmid_buffer = shmget(ENGINE_SHM_KEY, MAXBUF_char,
                                     write ? (IPC_CREAT | PERMS_WRITE) : PERMS_READ);
     if( shmid_buffer == -1)
-      	return 0;
+      	return nullptr;
 
-    shared_mem = (unsigned int*)shmat(shmid_buffer, 0, 0);
-    if( shared_mem == (void*)-1 ) {
-        shared_mem = 0;
-      	return 0;
+    void *mem = shmat(shmid_buffer, nullptr, 0);
+    if ( mem == (void*) -1 ) {
+        shared_mem = nullptr;
+      	return nullptr;
     }
+    shared_mem = reinterpret_cast<unsigned int*>(mem);
 
     if( write ) {
         // clear data buffer segment
@@ -59,3 +60,19 @@ bool engine_shm_detach()
     }
     return true;
 }
+
+// ########################################################################
+
+bool engine_shm_read(unsigned int *ptr, engine_shm_t *str)
+{
+    if ( !ptr )
+        return false;
+
+    str->time_us = &ptr[ENGINE_TIME_US];
+    str->time_s = &ptr[ENGINE_TIME_S];
+    str->data = ptr + ptr[ENGINE_DATA_START];
+    str->size = ptr[ENGINE_DATA_SIZE];
+    return true;
+}
+
+// ########################################################################
