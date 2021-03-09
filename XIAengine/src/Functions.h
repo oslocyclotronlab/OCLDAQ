eee53659fac1c8e42dbeaa16b6c341bd9614e44a
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2021-03-09 11:18:20 +0100

    Added a function that will try to read the number of PCI devices - should be equivalent to the number of XIA modules in the system

--- XIAengine/src/Functions.h
+++ XIAengine/src/Functions.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <vector>
+
 int AdjustBaselineOffset(unsigned short modNo);
+std::vector<unsigned short> ReadSlotMap(const char *ini_file = "pxisys.ini");
 
 #endif // FUNCTIONS_H
--- XIAengine/src/functions.cpp
+++ XIAengine/src/functions.cpp
@@ -3,6 +3,12 @@
 #include <pixie16app_export.h>
 #include <pixie16app_defs.h>
 
+#include <string>
+#include <array>
+#include <map>
+#include <sstream>
+
+#include <INIReader.h>
 
 int AdjustBaselineOffset(unsigned short modNo)
 {
@@ -29,4 +35,92 @@ int AdjustBaselineOffset(unsigned short modNo)
     }
 
     return retval;
+}
+
+// ##############################################
+// Some helper functions for the mapping function
+// ##############################################
+
+typedef struct {
+    int PCIBusNum;
+    int PCIDevNum;
+} pci_addr_t;
+
+bool operator<(const pci_addr_t &lhs, const pci_addr_t &rhs);
+std::string exec(const char* cmd);
+std::map<pci_addr_t, unsigned short> GetMapping(const char *ini_file = "pxisys.ini");
+
+
+
+std::vector<unsigned short> ReadSlotMap(const char *ini_file)
+{
+    auto map = GetMapping(ini_file);
+    auto devices = exec("lspci | grep \"PLX Technology, Inc. PCI9054 32-bit 33MHz PCI <-> IOBus Bridge (rev 0b)\"");
+    std::vector<unsigned short> slotMap;
+
+    // Next we need to decode the output. We read the string, line by line.
+    std::istringstream stream(devices);
+    std::string line;
+    while ( std::getline(stream, line) ){
+        // First we need to decode the first two characters
+        std::string PCIBusNumber(line.begin(), line.begin()+2);
+        std::string PCIDevNumber(line.begin()+3, line.begin()+5);
+
+        int busNum = std::stoi(PCIBusNumber, nullptr, 16);
+        int devNum = std::stoi(PCIDevNumber, nullptr, 16);
+
+
+        if ( map.find({busNum, devNum}) == map.end() ){
+            std::string errmsg = "Could not find PCI bus " + std::to_string(busNum);
+            errmsg += ", dev " + std::to_string(devNum);
+            throw std::runtime_error(errmsg);
+        }
+
+        slotMap.push_back(map[{busNum, devNum}]);
+    }
+    std::sort(std::begin(slotMap), std::end(slotMap));
+    return slotMap;
+}
+
+bool operator<(const pci_addr_t &lhs, const pci_addr_t &rhs)
+{
+    if ( lhs.PCIBusNum == rhs.PCIBusNum )
+        return ( lhs.PCIDevNum < rhs.PCIDevNum );
+    return (lhs.PCIBusNum < rhs.PCIBusNum);
+}
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer{};
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+std::map<pci_addr_t, unsigned short> GetMapping(const char *ini_file)
+{
+    INIReader reader(ini_file);
+    std::string slot_name;
+
+    if ( reader.ParseError() != 0 ){
+        std::string errmsg = "Could not load '" + std::string(ini_file) + "'";
+        throw std::runtime_error(errmsg);
+    }
+    std::map<pci_addr_t, unsigned short> map;
+    for ( unsigned short slot = 2 ; slot <= 14 ; ++slot ){
+        slot_name = "Slot" + std::to_string(slot);
+        int busNum = reader.GetInteger(slot_name, "PCIBusNumber", -1);
+        int devNum = reader.GetInteger(slot_name, "PCIDeviceNumber", -1);
+        if ( busNum < 0 || devNum < 0 ){
+            std::string errmsg = "Could read Bus/Dev number for section '" + std::string(slot_name) + "'";
+            throw std::runtime_error(errmsg);
+        }
+        map[{busNum, devNum}] = slot;
+    }
+    return map;
 }
