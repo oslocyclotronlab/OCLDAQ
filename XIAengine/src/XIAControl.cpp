82d9d46fe4d16b0e685f3947f8cd9c0d4fa8dfd0
Vetle Wegner Ingeberg <v.w.ingeberg@fys.uio.no>
2021-04-02 12:29:52 +0200

    Decreased time between emitting scalers to only 1 second

--- XIAengine/src/XIAControl.cpp
+++ XIAengine/src/XIAControl.cpp
@@ -97,7 +97,7 @@ bool XIAControl::XIA_check_buffer(int bufsize)
     timeval tmp;
     gettimeofday(&tmp, NULL);
     t2 = tmp.tv_sec + 1e-6*tmp.tv_usec;
-    if (t2 - t1 > 5 ){
+    if (t2 - t1 > 1.0 ){
         if (!WriteScalers())
             StopRun();
         last_time = tmp;
