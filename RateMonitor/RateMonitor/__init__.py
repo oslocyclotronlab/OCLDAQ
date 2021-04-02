b4fbaddfb803a35dfa4e094482bc7aa81883e281
Vetle Wegner Ingeberg <v.w.ingeberg@fys.uio.no>
2021-04-02 12:17:37 +0200

    Debug

--- RateMonitor/RateMonitor/__init__.py
+++ RateMonitor/RateMonitor/__init__.py
@@ -10,11 +10,10 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelna
 output_handler = logging.StreamHandler()
 systemd_handler = None
 try:
-    import cysystemd
-    systemd_handler = cysystemd.journal.JournaldLogHandler()
+    from cysystemd import journal
+    systemd_handler = journal.JournaldLogHandler()
     systemd_handler.setFormatter(formatter)
 except Exception:
-    print("Unable to locate systemd handler")
     pass
 
 
