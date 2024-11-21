0cfcf5386e849795a87f7f69f7fc076470b4813d
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2024-11-21 17:40:29 +0100

    Same treatment with pixie16_globals.h

--- Pixie16/src/pixie16app_globals.h
+++ Pixie16/src/pixie16app_globals.h
@@ -61,9 +61,9 @@ extern "C" {
 #endif
 
 
-unsigned short Number_Modules;                       // Total number of modules in the crate
-unsigned short Random_Set[RANDOMINDICES_LENGTH];     // Random indices
-unsigned short Offline;   // Offline = 1: offline analysis; Offline = 0: online analysis
+extern unsigned short Number_Modules;                       // Total number of modules in the crate
+extern unsigned short Random_Set[RANDOMINDICES_LENGTH];     // Random indices
+extern unsigned short Offline;   // Offline = 1: offline analysis; Offline = 0: online analysis
 
 
 /*-----------------------------------------------------------------
@@ -79,7 +79,7 @@ struct Pixie_Configuration
 };
 
 // Define PRESET_MAX_MODULES Pixie devices
-struct Pixie_Configuration Pixie_Devices[PRESET_MAX_MODULES];
+extern struct Pixie_Configuration Pixie_Devices[PRESET_MAX_MODULES];
 
 
 struct Module_Info
@@ -92,7 +92,7 @@ struct Module_Info
 };
 
 // Define PRESET_MAX_MODULES Pixie devices
-struct Module_Info Module_Information[PRESET_MAX_MODULES];
+extern struct Module_Info Module_Information[PRESET_MAX_MODULES];
 
 
 /*-----------------------------------------------------------------
@@ -105,143 +105,143 @@ struct Module_Info Module_Information[PRESET_MAX_MODULES];
 //	Module parameters
 //--------------------
 
-unsigned int ModNum_Address[PRESET_MAX_MODULES];         // ModNum
-unsigned int ModCSRA_Address[PRESET_MAX_MODULES];        // ModCSRA
-unsigned int ModCSRB_Address[PRESET_MAX_MODULES];        // ModCSRB
-unsigned int ModFormat_Address[PRESET_MAX_MODULES];      // ModFormat
-unsigned int RunTask_Address[PRESET_MAX_MODULES];        // RunTask
-unsigned int ControlTask_Address[PRESET_MAX_MODULES];    // ControlTask
-unsigned int MaxEvents_Address[PRESET_MAX_MODULES];      // MaxEvents
-unsigned int CoincPattern_Address[PRESET_MAX_MODULES];   // CoincPattern
-unsigned int CoincWait_Address[PRESET_MAX_MODULES];      // CoincWait
-unsigned int SynchWait_Address[PRESET_MAX_MODULES];      // SynchWait
-unsigned int InSynch_Address[PRESET_MAX_MODULES];        // InSynch
-unsigned int Resume_Address[PRESET_MAX_MODULES];         // Resume
-unsigned int SlowFilterRange_Address[PRESET_MAX_MODULES];// SlowFilterRange
-unsigned int FastFilterRange_Address[PRESET_MAX_MODULES];// FastFilterRange
-unsigned int ChanNum_Address[PRESET_MAX_MODULES];        // ChanNum
-unsigned int HostIO_Address[PRESET_MAX_MODULES];         // HostIO
-unsigned int UserIn_Address[PRESET_MAX_MODULES];         // UserIn
-unsigned int U00_Address[PRESET_MAX_MODULES];            // U00
-unsigned int FastTrigBackplaneEna_Address[PRESET_MAX_MODULES]; // Enabling sending fast trigger to backplane
-unsigned int CrateID_Address             [PRESET_MAX_MODULES]; // CrateID
-unsigned int SlotID_Address              [PRESET_MAX_MODULES]; // SlotID
-unsigned int ModID_Address               [PRESET_MAX_MODULES]; // ModID
-unsigned int TrigConfig_Address          [PRESET_MAX_MODULES]; // General configuration registers
-unsigned int HRTP_Address[PRESET_MAX_MODULES];           // HostRunTimePreset
+extern unsigned int ModNum_Address[PRESET_MAX_MODULES];         // ModNum
+extern unsigned int ModCSRA_Address[PRESET_MAX_MODULES];        // ModCSRA
+extern unsigned int ModCSRB_Address[PRESET_MAX_MODULES];        // ModCSRB
+extern unsigned int ModFormat_Address[PRESET_MAX_MODULES];      // ModFormat
+extern unsigned int RunTask_Address[PRESET_MAX_MODULES];        // RunTask
+extern unsigned int ControlTask_Address[PRESET_MAX_MODULES];    // ControlTask
+extern unsigned int MaxEvents_Address[PRESET_MAX_MODULES];      // MaxEvents
+extern unsigned int CoincPattern_Address[PRESET_MAX_MODULES];   // CoincPattern
+extern unsigned int CoincWait_Address[PRESET_MAX_MODULES];      // CoincWait
+extern unsigned int SynchWait_Address[PRESET_MAX_MODULES];      // SynchWait
+extern unsigned int InSynch_Address[PRESET_MAX_MODULES];        // InSynch
+extern unsigned int Resume_Address[PRESET_MAX_MODULES];         // Resume
+extern unsigned int SlowFilterRange_Address[PRESET_MAX_MODULES];// SlowFilterRange
+extern unsigned int FastFilterRange_Address[PRESET_MAX_MODULES];// FastFilterRange
+extern unsigned int ChanNum_Address[PRESET_MAX_MODULES];        // ChanNum
+extern unsigned int HostIO_Address[PRESET_MAX_MODULES];         // HostIO
+extern unsigned int UserIn_Address[PRESET_MAX_MODULES];         // UserIn
+extern unsigned int U00_Address[PRESET_MAX_MODULES];            // U00
+extern unsigned int FastTrigBackplaneEna_Address[PRESET_MAX_MODULES]; // Enabling sending fast trigger to backplane
+extern unsigned int CrateID_Address             [PRESET_MAX_MODULES]; // CrateID
+extern unsigned int SlotID_Address              [PRESET_MAX_MODULES]; // SlotID
+extern unsigned int ModID_Address               [PRESET_MAX_MODULES]; // ModID
+extern unsigned int TrigConfig_Address          [PRESET_MAX_MODULES]; // General configuration registers
+extern unsigned int HRTP_Address[PRESET_MAX_MODULES];           // HostRunTimePreset
 
 //--------------------
 //	Channel parameters
 //--------------------
 
-unsigned int ChanCSRa_Address[PRESET_MAX_MODULES];       // ChanCSRa
-unsigned int ChanCSRb_Address[PRESET_MAX_MODULES];       // ChanCSRb
+extern unsigned int ChanCSRa_Address[PRESET_MAX_MODULES];       // ChanCSRa
+extern unsigned int ChanCSRb_Address[PRESET_MAX_MODULES];       // ChanCSRb
 
-unsigned int GainDAC_Address[PRESET_MAX_MODULES];        // GainDAC
+extern unsigned int GainDAC_Address[PRESET_MAX_MODULES];        // GainDAC
 
-unsigned int OffsetDAC_Address[PRESET_MAX_MODULES];      // OffsetDAC
+extern unsigned int OffsetDAC_Address[PRESET_MAX_MODULES];      // OffsetDAC
 
-unsigned int DigGain_Address[PRESET_MAX_MODULES];        // DigGain
+extern unsigned int DigGain_Address[PRESET_MAX_MODULES];        // DigGain
 
-unsigned int SlowLength_Address[PRESET_MAX_MODULES];     // SlowLength
-unsigned int SlowGap_Address[PRESET_MAX_MODULES];        // SlowGap
-unsigned int FastLength_Address[PRESET_MAX_MODULES];     // FastLength
-unsigned int FastGap_Address[PRESET_MAX_MODULES];        // FastGap
-unsigned int PeakSample_Address[PRESET_MAX_MODULES];     // PeakSample
-unsigned int PeakSep_Address[PRESET_MAX_MODULES];        // PeakSep
+extern unsigned int SlowLength_Address[PRESET_MAX_MODULES];     // SlowLength
+extern unsigned int SlowGap_Address[PRESET_MAX_MODULES];        // SlowGap
+extern unsigned int FastLength_Address[PRESET_MAX_MODULES];     // FastLength
+extern unsigned int FastGap_Address[PRESET_MAX_MODULES];        // FastGap
+extern unsigned int PeakSample_Address[PRESET_MAX_MODULES];     // PeakSample
+extern unsigned int PeakSep_Address[PRESET_MAX_MODULES];        // PeakSep
 
-unsigned int CFDThresh_Address[PRESET_MAX_MODULES];      // CFDThresh
+extern unsigned int CFDThresh_Address[PRESET_MAX_MODULES];      // CFDThresh
 
-unsigned int FastThresh_Address[PRESET_MAX_MODULES];     // FastThresh
-unsigned int ThreshWidth_Address[PRESET_MAX_MODULES];    // ThreshWidth
-unsigned int PAFlength_Address[PRESET_MAX_MODULES];      // PAFlength
-unsigned int TriggerDelay_Address[PRESET_MAX_MODULES];   // TriggerDelay
-unsigned int ResetDelay_Address[PRESET_MAX_MODULES];     // ResetDelay
-unsigned int ChanTrigStretch_Address[PRESET_MAX_MODULES];// ChanTrigStretch
-unsigned int TraceLength_Address[PRESET_MAX_MODULES];    // TraceLength
-unsigned int TrigOutLen_Address[PRESET_MAX_MODULES];     // TrigOutLen
-unsigned int EnergyLow_Address[PRESET_MAX_MODULES];      // EnergyLow
-unsigned int Log2Ebin_Address[PRESET_MAX_MODULES];       // Log2Ebin
+extern unsigned int FastThresh_Address[PRESET_MAX_MODULES];     // FastThresh
+extern unsigned int ThreshWidth_Address[PRESET_MAX_MODULES];    // ThreshWidth
+extern unsigned int PAFlength_Address[PRESET_MAX_MODULES];      // PAFlength
+extern unsigned int TriggerDelay_Address[PRESET_MAX_MODULES];   // TriggerDelay
+extern unsigned int ResetDelay_Address[PRESET_MAX_MODULES];     // ResetDelay
+extern unsigned int ChanTrigStretch_Address[PRESET_MAX_MODULES];// ChanTrigStretch
+extern unsigned int TraceLength_Address[PRESET_MAX_MODULES];    // TraceLength
+extern unsigned int TrigOutLen_Address[PRESET_MAX_MODULES];     // TrigOutLen
+extern unsigned int EnergyLow_Address[PRESET_MAX_MODULES];      // EnergyLow
+extern unsigned int Log2Ebin_Address[PRESET_MAX_MODULES];       // Log2Ebin
 
-unsigned int MultiplicityMaskL_Address[PRESET_MAX_MODULES]; // Multiplicity contribution mask - low 32-bit
+extern unsigned int MultiplicityMaskL_Address[PRESET_MAX_MODULES]; // Multiplicity contribution mask - low 32-bit
 
-unsigned int PSAoffset_Address[PRESET_MAX_MODULES];      // PSAoffset
-unsigned int PSAlength_Address[PRESET_MAX_MODULES];      // PSAlength
-unsigned int Integrator_Address[PRESET_MAX_MODULES];     // Integrator
+extern unsigned int PSAoffset_Address[PRESET_MAX_MODULES];      // PSAoffset
+extern unsigned int PSAlength_Address[PRESET_MAX_MODULES];      // PSAlength
+extern unsigned int Integrator_Address[PRESET_MAX_MODULES];     // Integrator
 
-unsigned int BLcut_Address[PRESET_MAX_MODULES];          // BLcut
-unsigned int BaselinePercent_Address[PRESET_MAX_MODULES];// BaselinePercent
+extern unsigned int BLcut_Address[PRESET_MAX_MODULES];          // BLcut
+extern unsigned int BaselinePercent_Address[PRESET_MAX_MODULES];// BaselinePercent
 
-unsigned int FtrigoutDelay_Address[PRESET_MAX_MODULES];  // Fast trigger output delay for system synchronization
+extern unsigned int FtrigoutDelay_Address[PRESET_MAX_MODULES];  // Fast trigger output delay for system synchronization
 
-unsigned int Log2Bweight_Address[PRESET_MAX_MODULES];    // Log2Bweight
-unsigned int PreampTau_Address[PRESET_MAX_MODULES];      // PreampTau
+extern unsigned int Log2Bweight_Address[PRESET_MAX_MODULES];    // Log2Bweight
+extern unsigned int PreampTau_Address[PRESET_MAX_MODULES];      // PreampTau
 
-unsigned int MultiplicityMaskH_Address[PRESET_MAX_MODULES]; // Multiplicity contribution mask - high 32-bit
+extern unsigned int MultiplicityMaskH_Address[PRESET_MAX_MODULES]; // Multiplicity contribution mask - high 32-bit
 
-unsigned int FastTrigBackLen_Address[PRESET_MAX_MODULES];	// FastTrigBackLen
+extern unsigned int FastTrigBackLen_Address[PRESET_MAX_MODULES];	// FastTrigBackLen
 
-unsigned int CFDDelay_Address      [PRESET_MAX_MODULES];	// CFD delay
-unsigned int CFDScale_Address      [PRESET_MAX_MODULES];	// CFD scale
-unsigned int ExternDelayLen_Address[PRESET_MAX_MODULES];	// delay length for each channel's input signal
-unsigned int ExtTrigStretch_Address[PRESET_MAX_MODULES];	// external trigger stretch
-unsigned int VetoStretch_Address   [PRESET_MAX_MODULES];	// veto stretch
-unsigned int QDCLen0_Address       [PRESET_MAX_MODULES];	// QDC #0 length
-unsigned int QDCLen1_Address       [PRESET_MAX_MODULES];	// QDC #1 length
-unsigned int QDCLen2_Address       [PRESET_MAX_MODULES];	// QDC #2 length
-unsigned int QDCLen3_Address       [PRESET_MAX_MODULES];	// QDC #3 length
-unsigned int QDCLen4_Address       [PRESET_MAX_MODULES];	// QDC #4 length
-unsigned int QDCLen5_Address       [PRESET_MAX_MODULES];	// QDC #5 length
-unsigned int QDCLen6_Address       [PRESET_MAX_MODULES];	// QDC #6 length
-unsigned int QDCLen7_Address       [PRESET_MAX_MODULES];	// QDC #7 length
+extern unsigned int CFDDelay_Address      [PRESET_MAX_MODULES];	// CFD delay
+extern unsigned int CFDScale_Address      [PRESET_MAX_MODULES];	// CFD scale
+extern unsigned int ExternDelayLen_Address[PRESET_MAX_MODULES];	// delay length for each channel's input signal
+extern unsigned int ExtTrigStretch_Address[PRESET_MAX_MODULES];	// external trigger stretch
+extern unsigned int VetoStretch_Address   [PRESET_MAX_MODULES];	// veto stretch
+extern unsigned int QDCLen0_Address       [PRESET_MAX_MODULES];	// QDC #0 length
+extern unsigned int QDCLen1_Address       [PRESET_MAX_MODULES];	// QDC #1 length
+extern unsigned int QDCLen2_Address       [PRESET_MAX_MODULES];	// QDC #2 length
+extern unsigned int QDCLen3_Address       [PRESET_MAX_MODULES];	// QDC #3 length
+extern unsigned int QDCLen4_Address       [PRESET_MAX_MODULES];	// QDC #4 length
+extern unsigned int QDCLen5_Address       [PRESET_MAX_MODULES];	// QDC #5 length
+extern unsigned int QDCLen6_Address       [PRESET_MAX_MODULES];	// QDC #6 length
+extern unsigned int QDCLen7_Address       [PRESET_MAX_MODULES];	// QDC #7 length
 
-unsigned int Xwait_Address[PRESET_MAX_MODULES];				// Xwait
+extern unsigned int Xwait_Address[PRESET_MAX_MODULES];				// Xwait
 
 
 //--------------------
 //	Results parameters
 //--------------------
 
-unsigned int RealTimeA_Address[PRESET_MAX_MODULES];    // RealTimeA
-unsigned int RealTimeB_Address[PRESET_MAX_MODULES];    // RealTimeB
-unsigned int RunTimeA_Address[PRESET_MAX_MODULES];     // RunTimeA
-unsigned int RunTimeB_Address[PRESET_MAX_MODULES];     // RunTimeB
-unsigned int GSLTtime_Address[PRESET_MAX_MODULES];     // GSLTtime
-unsigned int NumEventsA_Address[PRESET_MAX_MODULES];   // NumEventsA
-unsigned int NumEventsB_Address[PRESET_MAX_MODULES];   // NumEventsB
-unsigned int DSPerror_Address[PRESET_MAX_MODULES];     // DSPerror
-unsigned int SynchDone_Address[PRESET_MAX_MODULES];    // SynchDone
-unsigned int BufHeadLen_Address[PRESET_MAX_MODULES];   // BufHeadLen
-unsigned int EventHeadLen_Address[PRESET_MAX_MODULES]; // EventHeadLen
-unsigned int ChanHeadLen_Address[PRESET_MAX_MODULES];  // ChanHeadLen
-unsigned int UserOut_Address[PRESET_MAX_MODULES];      // UserOut
-unsigned int AOutBuffer_Address[PRESET_MAX_MODULES];   // AOutBuffer
-unsigned int LOutBuffer_Address[PRESET_MAX_MODULES];   // LOutBuffer
-unsigned int AECorr_Address[PRESET_MAX_MODULES];       // AECorr
-unsigned int LECorr_Address[PRESET_MAX_MODULES];       // LECorr
-unsigned int HardwareID_Address[PRESET_MAX_MODULES];   // HardwareID
-unsigned int HardVariant_Address[PRESET_MAX_MODULES];  // HardVariant
-unsigned int FIFOLength_Address[PRESET_MAX_MODULES];   // FIFOLength
-unsigned int FippiID_Address[PRESET_MAX_MODULES];      // FippiID
-unsigned int FippiVariant_Address[PRESET_MAX_MODULES]; // FippiVariant
-unsigned int DSPrelease_Address[PRESET_MAX_MODULES];   // DSPrelease
-unsigned int DSPbuild_Address[PRESET_MAX_MODULES];     // DSPbuild
-unsigned int DSPVariant_Address[PRESET_MAX_MODULES];   // DSPVariant
-unsigned int U20_Address[PRESET_MAX_MODULES];          // U20
-unsigned int LiveTimeA_Address[PRESET_MAX_MODULES];    // LiveTimeA
-unsigned int LiveTimeB_Address[PRESET_MAX_MODULES];    // LiveTimeB
-unsigned int FastPeaksA_Address[PRESET_MAX_MODULES];   // FastPeaksA
-unsigned int FastPeaksB_Address[PRESET_MAX_MODULES];   // FastPeaksB
-unsigned int OverflowA_Address[PRESET_MAX_MODULES];    // OverflowA
-unsigned int OverflowB_Address[PRESET_MAX_MODULES];    // OverflowB
-unsigned int InSpecA_Address[PRESET_MAX_MODULES];      // InSpecA
-unsigned int InSpecB_Address[PRESET_MAX_MODULES];      // InSpecB
-unsigned int UnderflowA_Address[PRESET_MAX_MODULES];   // UnderflowA
-unsigned int UnderflowB_Address[PRESET_MAX_MODULES];   // UnderflowB
-unsigned int ChanEventsA_Address[PRESET_MAX_MODULES];  // ChanEventsA
-unsigned int ChanEventsB_Address[PRESET_MAX_MODULES];  // ChanEventsB
-unsigned int AutoTau_Address[PRESET_MAX_MODULES];      // AutoTau
-unsigned int U30_Address[PRESET_MAX_MODULES];          // U30
+extern unsigned int RealTimeA_Address[PRESET_MAX_MODULES];    // RealTimeA
+extern unsigned int RealTimeB_Address[PRESET_MAX_MODULES];    // RealTimeB
+extern unsigned int RunTimeA_Address[PRESET_MAX_MODULES];     // RunTimeA
+extern unsigned int RunTimeB_Address[PRESET_MAX_MODULES];     // RunTimeB
+extern unsigned int GSLTtime_Address[PRESET_MAX_MODULES];     // GSLTtime
+extern unsigned int NumEventsA_Address[PRESET_MAX_MODULES];   // NumEventsA
+extern unsigned int NumEventsB_Address[PRESET_MAX_MODULES];   // NumEventsB
+extern unsigned int DSPerror_Address[PRESET_MAX_MODULES];     // DSPerror
+extern unsigned int SynchDone_Address[PRESET_MAX_MODULES];    // SynchDone
+extern unsigned int BufHeadLen_Address[PRESET_MAX_MODULES];   // BufHeadLen
+extern unsigned int EventHeadLen_Address[PRESET_MAX_MODULES]; // EventHeadLen
+extern unsigned int ChanHeadLen_Address[PRESET_MAX_MODULES];  // ChanHeadLen
+extern unsigned int UserOut_Address[PRESET_MAX_MODULES];      // UserOut
+extern unsigned int AOutBuffer_Address[PRESET_MAX_MODULES];   // AOutBuffer
+extern unsigned int LOutBuffer_Address[PRESET_MAX_MODULES];   // LOutBuffer
+extern unsigned int AECorr_Address[PRESET_MAX_MODULES];       // AECorr
+extern unsigned int LECorr_Address[PRESET_MAX_MODULES];       // LECorr
+extern unsigned int HardwareID_Address[PRESET_MAX_MODULES];   // HardwareID
+extern unsigned int HardVariant_Address[PRESET_MAX_MODULES];  // HardVariant
+extern unsigned int FIFOLength_Address[PRESET_MAX_MODULES];   // FIFOLength
+extern unsigned int FippiID_Address[PRESET_MAX_MODULES];      // FippiID
+extern unsigned int FippiVariant_Address[PRESET_MAX_MODULES]; // FippiVariant
+extern unsigned int DSPrelease_Address[PRESET_MAX_MODULES];   // DSPrelease
+extern unsigned int DSPbuild_Address[PRESET_MAX_MODULES];     // DSPbuild
+extern unsigned int DSPVariant_Address[PRESET_MAX_MODULES];   // DSPVariant
+extern unsigned int U20_Address[PRESET_MAX_MODULES];          // U20
+extern unsigned int LiveTimeA_Address[PRESET_MAX_MODULES];    // LiveTimeA
+extern unsigned int LiveTimeB_Address[PRESET_MAX_MODULES];    // LiveTimeB
+extern unsigned int FastPeaksA_Address[PRESET_MAX_MODULES];   // FastPeaksA
+extern unsigned int FastPeaksB_Address[PRESET_MAX_MODULES];   // FastPeaksB
+extern unsigned int OverflowA_Address[PRESET_MAX_MODULES];    // OverflowA
+extern unsigned int OverflowB_Address[PRESET_MAX_MODULES];    // OverflowB
+extern unsigned int InSpecA_Address[PRESET_MAX_MODULES];      // InSpecA
+extern unsigned int InSpecB_Address[PRESET_MAX_MODULES];      // InSpecB
+extern unsigned int UnderflowA_Address[PRESET_MAX_MODULES];   // UnderflowA
+extern unsigned int UnderflowB_Address[PRESET_MAX_MODULES];   // UnderflowB
+extern unsigned int ChanEventsA_Address[PRESET_MAX_MODULES];  // ChanEventsA
+extern unsigned int ChanEventsB_Address[PRESET_MAX_MODULES];  // ChanEventsB
+extern unsigned int AutoTau_Address[PRESET_MAX_MODULES];      // AutoTau
+extern unsigned int U30_Address[PRESET_MAX_MODULES];          // U30
 
 
 
