6493a101fd952c25aa5f70ada61c03958af439e4
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2025-08-18 20:44:50 +0200

    added a rough gain and shift

--- XIAonline/src/Sort_Funct.cpp
+++ XIAonline/src/Sort_Funct.cpp
@@ -14,6 +14,10 @@ static calibration_t calibration;
 
 calibration_t *GetCalibration(){ return &calibration; }
 
+
+static const double gain[] = {0.532786, 0.593626, 0.606949, 0.483961, 0.32795, 0.800924, 0.306407, 0.335072, 0.264426, 0.411669, 0.325721, 0.403113};
+static const double shift[] = {54.1865, 13.2793, 13.2547, 24.4136, 97.8915, 54.1035, 18.9814, 82.467, 51.2308, 44.687, 44.8648, 60.4771};
+
 void sort_singles(const std::vector<word_t> &buffer)
 {
 
@@ -27,7 +31,8 @@ void sort_singles(const std::vector<word_t> &buffer)
         case labr:
             spec_fill(LABRSP_ID, entry.adcdata, dinfo.detectorNum);
             spec_fill(LABRCSP_ID,
-                      calibration.gain_labr[dinfo.detectorNum]*(entry.adcdata + drand48() - 0.5) + calibration.shift_labr[dinfo.detectorNum],
+                      gain[dinfo.detectorNum]*(entry.adcdata + drand48() - 0.5) + shift[dinfo.detectorNum],
+                      //calibration.gain_labr[dinfo.detectorNum]*(entry.adcdata + drand48() - 0.5) + calibration.shift_labr[dinfo.detectorNum],
                       dinfo.detectorNum);
             if ( entry.cfdfail )
                 spec_fill(LABRCFD_ID, entry.adcdata, dinfo.detectorNum);
