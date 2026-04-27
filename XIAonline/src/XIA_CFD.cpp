6525c1654b045c727029cb8c397c56c4c2a68d60
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2026-04-27 11:30:11 +0200

    Updated XIA CFD algorithms
    Ive copied the latest version of the XIA CFD functions from vetlewi/XIA2tree.

--- XIAonline/src/XIA_CFD.cpp
+++ XIAonline/src/XIA_CFD.cpp
@@ -1,102 +1,93 @@
 #include "XIA_CFD.h"
 
+#include <stdexcept>
 
+using namespace XIA;
 
-#include <stdlib.h>
-#include <stdint.h>
+struct XIA_CFD_base_t {
+    virtual explicit operator bool() const noexcept = 0;
+    virtual explicit operator double() const noexcept = 0;
+};
 
-#define BIT15TO15	0x8000
-#define BIT14TO14	0x4000
-#define BIT15TO13	0xE000
-#define BIT14TO0	0x7FFF
-#define BIT13TO0	0x3FFF
-#define BIT12TO0	0x1FFF
+struct XIA_CFD_100MHz_t {
+  unsigned short timecfd : 15;
+  bool fail : 1;
 
-double XIA_CFD_Fraction_100MHz(uint16_t CFDvalue, bool* fail)
-{
-	double correction;
-	uint32_t cfdfailbit, timecfd;
+  inline explicit operator bool() const noexcept { return fail; }
+  inline explicit operator double() const noexcept { return (fail) ? 0 : 10 * double(timecfd) / 32768.0; }
 
-	cfdfailbit = ((CFDvalue & BIT15TO15) >> 15);
-	timecfd = ((CFDvalue & BIT14TO0) >> 0);
-	if ( timecfd == 0 ){
-		correction = 0;
-		*fail = true;
-		return correction;
-	}
+  inline double get_time() const noexcept { return ( fail ) ? 0 : 10 * double(timecfd) / 32768.0; }
+  inline bool get_fail() const noexcept { return fail; }
+};
 
-	correction = (10*(double)timecfd)/32768.0;
-    *fail = ( cfdfailbit > 0 );
-	return correction;
+struct XIA_CFD_250MHz_t {
+    unsigned short timecfd : 14;
+    unsigned short trigsource : 1;
+    bool fail : 1;
+
+    inline explicit operator bool() const noexcept { return fail; }
+    inline explicit operator double() const { return (fail) ? 0 : 4 * (double(timecfd) / 16384.0 - trigsource); }
+
+    inline double get_time() const noexcept { return (fail) ? 0 : 4 * (double(timecfd) / 16384.0 - trigsource); }
+    inline bool get_fail() const noexcept { return fail; }
+};
+
+struct XIA_CFD_500MHz_t {
+    unsigned short timecfd : 13;
+    unsigned short trigsource : 3;
+
+    inline explicit operator bool() const { return (trigsource >= 7); }
+    inline explicit operator double() const {
+        return (trigsource >= 7) ? 0 : 2 * ( timecfd/8192.0 + trigsource - 1.);
+    }
+
+    inline double get_time() const noexcept { return (trigsource >= 7) ? 0 : 2 * (double(timecfd) / 8192.0 + trigsource - 1.); }
+    inline bool get_fail() const noexcept { return (trigsource >= 7); }
+};
+
+double XIA::XIA_CFD_Fraction_100MHz(const uint16_t &CFDvalue, bool &fail) {
+  auto *cfd = reinterpret_cast<const XIA_CFD_100MHz_t *>(&CFDvalue);
+  fail = cfd->fail;
+  return double(*cfd);
 }
 
-double XIA_CFD_Fraction_250MHz(uint16_t CFDvalue, bool* fail)
-{
-	double correction;
-	uint32_t cfdfailbit, cfdtrigsource, timecfd;
 
-	cfdfailbit = ((CFDvalue & BIT15TO15) >> 15);
-	cfdtrigsource = ((CFDvalue & BIT14TO14) >> 14);
-	timecfd = ((CFDvalue & BIT13TO0) >> 0);
 
-	if ( timecfd == 0 ){
-		correction = 0;
-		*fail = true;
-		return correction;
-	}
-
-	correction = (((double)timecfd)/16384.0 - cfdtrigsource)*4.0;
-    *fail = ( cfdfailbit > 0 );
-	return correction;
+double XIA::XIA_CFD_Fraction_250MHz(const uint16_t &CFDvalue, bool &fail) {
+  auto *cfd = reinterpret_cast<const XIA_CFD_250MHz_t *>(&CFDvalue);
+  fail = cfd->fail;
+  return double(*cfd);
 }
 
-double XIA_CFD_Fraction_500MHz(uint16_t CFDvalue, bool* fail)
-{
-	double correction;
-	uint32_t cfdtrigsource, timecfd;
 
-	cfdtrigsource = ((CFDvalue & BIT15TO13) >> 13);
-	timecfd = ((CFDvalue & BIT12TO0) >> 0);
-	correction = (((double)timecfd)/8192.0 + cfdtrigsource - 1.0)*2.0;
-    *fail = (cfdtrigsource > 4);
-	return correction;
+double XIA::XIA_CFD_Fraction_500MHz(const uint16_t &CFDvalue, bool &fail) {
+  auto *cfd = reinterpret_cast<const XIA_CFD_500MHz_t *>(&CFDvalue);
+  fail = bool(*cfd);
+  return double(*cfd);
 }
 
-double XIA_time_in_ns_100MHz(int64_t timestamp, uint16_t CFDvalue)
+XIA::XIA_CFD_t XIA::XIA_CFD_Decode(const ADCSamplingFreq &frequency, const uint16_t &CFDvalue)
 {
-	double correction, time_in_ns;
-    bool fail;
+    switch ( frequency ) {
 
-	correction = XIA_CFD_Fraction_100MHz(CFDvalue, &fail);
-	correction = fail ? 10.*((double)rand()/RAND_MAX) : correction;
+        default :
+            break;
 
-	time_in_ns = 10.0*(double)timestamp;
-	time_in_ns = time_in_ns + correction;
-	return time_in_ns;
-}
+        [[unlikely]] case f100MHz : {
+            auto *cfd = reinterpret_cast<const XIA_CFD_100MHz_t *>(&CFDvalue);
+            return std::make_pair(cfd->get_time(), cfd->get_fail());
+        }
 
-double XIA_time_in_ns_250MHz(int64_t timestamp, uint16_t CFDvalue)
-{
-	double correction, time_in_ns;
-    bool fail;
+        [[likely]] case f250MHz : {
+            auto *cfd = reinterpret_cast<const XIA_CFD_250MHz_t *>(&CFDvalue);
+            return std::make_pair(cfd->get_time(), cfd->get_fail());
+        }
 
-	correction = XIA_CFD_Fraction_250MHz(CFDvalue, &fail);
-	correction = fail ? 4.0*((double)rand()/RAND_MAX - 1) : correction;
-
-	time_in_ns = 8.0*(double)timestamp;
-	time_in_ns = time_in_ns + correction;
-	return time_in_ns;
-}
-
-double XIA_time_in_ns_500MHz(int64_t timestamp, uint16_t CFDvalue)
-{
-	double correction, time_in_ns;
-    bool fail;
-
-	correction = XIA_CFD_Fraction_500MHz(CFDvalue, &fail);
-	correction = fail ? 2.0*(7.*(double)rand()/RAND_MAX - 1) : correction;
-
-	time_in_ns = 10.0*(double)timestamp;
-	time_in_ns = time_in_ns + correction;
-	return time_in_ns;
+        [[likely]] case f500MHz :
+            auto *cfd = reinterpret_cast<const XIA_CFD_500MHz_t *>(&CFDvalue);
+            return std::make_pair(cfd->get_time(), cfd->get_fail());
+    }
+    //throw std::invalid_argument("Unknown frequency");
+    auto *cfd = reinterpret_cast<const XIA_CFD_100MHz_t *>(&CFDvalue);
+    return std::make_pair(cfd->get_time(), cfd->get_fail());
 }
