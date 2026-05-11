6ebffb6a2743dfca3917d5174b26d866d8de09a3
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2026-05-11 15:36:04 +0200

    Moved STRUCTOPT to main

--- rserver/options.h
+++ rserver/options.h
@@ -5,31 +5,32 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
-#include <structopt/app.hpp>
+//#include <structopt/app.hpp>
 
+#include <optional>
 #include <string>
 #include <logfault/logfault.h>
 
-struct Options_t {
+//namespace OCL {
+    struct Options_t {
 
-    // HTTP engine
-    std::optional<std::string> engine = "http";
+        // HTTP engine
+        std::optional<std::string> engine = "http";
 
-    // Optional bind address for the ROOT HTTP server.
-    // Default is 0.0.0.0. Maybe not the most secure...
-    std::optional<std::string> bind_address = "0.0.0.0";
+        // Optional bind address for the ROOT HTTP server.
+        // Default is 0.0.0.0. Maybe not the most secure...
+        std::optional<std::string> bind_address = "0.0.0.0";
 
-    // Optional bind port for the ROOT HTTP server.
-    // Default is 8686.
-    std::optional<size_t> bind_port = 8686;
+        // Optional bind port for the ROOT HTTP server.
+        // Default is 8686.
+        std::optional<size_t> bind_port = 8686;
 
-    // Define log level
-    std::optional<logfault::LogLevel> log_level = logfault::LogLevel::INFO;
+        // Define log level
+        std::optional<logfault::LogLevel> log_level = logfault::LogLevel::INFO;
 
-    // Name of shared histograms
-    std::optional<std::string> shared_histograms_name = "XIAonline";
-};
-
-STRUCTOPT(Options_t, engine, bind_address, bind_port, log_level, shared_histograms_name);
+        // Name of shared histograms
+        std::optional<std::string> shared_histograms_name = "XIAonline";
+    };
+//}
 
 #endif //OPTIONS_H
